8bc6e5afd363e2dab3b786d3cb9e7abc865edb43
Hassan Farid <hassan.seth@gmail.com>
2020-04-20 15:54:19 -0400

    Updated documentation generator to exclude tests and include all internal packages available

--- ci/generate_code_documentation.sh
+++ ci/generate_code_documentation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Generating reStructuresText..."
-sphinx-apidoc --full -o ../docs/ .
+sphinx-apidoc --full -o ../docs/ . test*
 
 cd ../docs/
+
 echo "Generating HTML output..."
 make clean html
