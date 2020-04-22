15ac4687ecd6997dc370d8392378d6c2295398e9
Hassan Farid <hassan.seth@gmail.com>
2020-04-22 17:29:21 -0400

    Updated ci/mypy configuration and execute script

--- ci/mypy/script.sh
+++ ci/mypy/script.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "Scanning module: gamedemo/"
-mypy --config-file ../ci/mypy/mypy_config.ini gamedemo/
-
-echo "Scanning module: olddemo/"
-mypy --config-file ../ci/mypy/mypy_config.ini olddemo/
-
-echo "Scanning module: game.py"
-mypy --config-file ../ci/mypy/mypy_config.ini game.py
+echo "Scanning all modules"
+mypy --config-file ../ci/mypy/mypy_config.ini .
